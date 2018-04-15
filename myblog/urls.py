eb8f411009a8763e18d098313e3d36b813593d53
Tanvir Mahmud Emon <emontme@gmail.com>
2018-04-15 07:49:14 +0600

    Add files via upload

---
+++ myblog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^myblog/$', views.post_list, name='post_list'),  # http://127.0.0.1:8000/myblog/myblog/
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+]
